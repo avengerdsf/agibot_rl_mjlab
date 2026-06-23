aeac6e376689853442a47956e0f9260dad286cac
yinhong <1329312243@qq.com>
2026-06-23 21:14:12 +0800

    fix

--- agibot_rl/tasks/velocity/config/x1/env_cfgs.py
+++ agibot_rl/tasks/velocity/config/x1/env_cfgs.py
@@ -319,15 +319,12 @@ def agibot_x1_flat_env_cfg(play: bool = False) -> ManagerBasedRlEnvCfg:
   cfg.observations["critic"].enable_corruption = False
 
 
-  # cfg.rewards["track_linear_velocity"].weight = 0.3
-  # cfg.rewards["track_angular_velocity"].weight = 0.3
+  cfg.rewards["track_linear_velocity"].weight = 0.3
+  cfg.rewards["track_angular_velocity"].weight = 0.3
   # cfg.rewards["track_linear_velocity"].params["std"] = math.sqrt(0.16)
   # cfg.rewards["track_angular_velocity"].params["std"] = math.sqrt(0.1)
   # cfg.rewards["track_vel_hard"].params["sigma_v"] = 0.30
-  # cfg.rewards["track_vel_hard"].weight = 0.0
-  cfg.rewards["track_linear_velocity"] = None
-  cfg.rewards["track_angular_velocity"] = None
-  cfg.rewards["track_vel_hard"]  = None
+  cfg.rewards["track_vel_hard"].weight = 0.0
 
 
 
